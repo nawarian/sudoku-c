d61a164f8b6293df0efc0d83a3955a5650f88913
Nawarian <nickolas@phpsp.org.br>
2023-06-26 07:37:39 -0300

    chore: receive grid via param

--- src/main.c
+++ src/main.c
@@ -1,22 +1,17 @@
 #include <raylib.h>
+#include <string.h>
 
 #include "grid.h"
 #include "globals.h"
 
-int main(void)
+int main(int argc, const char **argv)
 {
-/*  6, 9, 0,  8, 0, 5,  7, 3, 0,
-    5, 1, 8,  7, 6, 3,  0, 2, 0,
-    3, 7, 4,  1, 9, 2,  6, 8, 0,
+    if (argc < 2) {
+        TraceLog(LOG_FATAL, "Usage: solver solver-string");
+    }
 
-    0, 0, 7,  9, 5, 4,  3, 1, 2,
-    9, 3, 0,  2, 1, 7,  0, 4, 6,
-    4, 2, 1,  6, 3, 8,  9, 0, 0,
-
-    0, 5, 6,  3, 8, 1,  2, 9, 4,
-    0, 4, 0,  5, 7, 9,  1, 6, 8,
-    0, 8, 9,  4, 0, 6,  0, 7, 3  */
-    char matrix[9*9] = "690805730518763020374192680007954312930217046421638900056381294040579168089406073";
+    char matrix[9*9];
+    memcpy(matrix, argv[1], sizeof(matrix));
 
     grid_t grid = grid_load(matrix);
 
